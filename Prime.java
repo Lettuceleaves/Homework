3c8e816e15bfc26957167f3f93d7c645a1128439
LettuceLeaves <2878506229@qq.com>
2025-10-08 12:46:12 +0800

    feature:软件工程实验一

---
+++ Prime.java
@@ -0,0 +1,38 @@
+public class Prime {
+    public static void sieveAndPrint(int limit, int perLine) {
+        if (limit < 2) {
+            return;
+        }
+        boolean[] isPrime = new boolean[limit + 1];
+        for (int i = 2; i <= limit; i++) {
+            isPrime[i] = true;
+        }
+        for (int p = 2; p * p <= limit; p++) {
+            // 如果 isPrime[p] 仍为 true，则 p 是素数
+            if (isPrime[p]) {
+                for (int i = p * p; i <= limit; i += p) {
+                    isPrime[i] = false;
+                }
+            }
+        }
+        int count = 0;
+
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime[i]) {
+                System.out.printf("%d", i);
+                count++;
+                if (count % perLine == 0) {
+                    System.out.println();
+                }
+            }
+        }
+
+        if (count % perLine != 0) {
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        sieveAndPrint(20000, 5);
+    }
+}
